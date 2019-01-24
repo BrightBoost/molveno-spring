435876fbf715aa20986f7d54bdba8d24cd794648
husam30 <38981010+husam30@users.noreply.github.com>
2019-01-24 14:18:26 +0100

     Change the logo  Remove nationality from form  Homepage slider: slower  Review stars gold and smaller  Smaller guest info form

---
+++ src/main/resources/static/restaurant/dist/demo.js
@@ -0,0 +1,37 @@
+$('document').ready(function() {
+        $('.menu').each(function() {
+            $(this).css("display","none");});
+    $('#M1').click(function(){
+        $('.menu').each(function() {
+            $(this).css("display","none");});
+            $('#tabelM1').css("display","block");
+        });
+    $('#M2').click(function(){
+            $('.menu').each(function() {
+                $(this).css("display","none");});
+            $('#tabelM2').css("display","block");
+        });
+    $('#M3').click(function(){
+            $('.menu').each(function() {
+                $(this).css("display","none");});
+            $('#tabelM3').css("display","block");
+        });
+            $('#M4').click(function(){
+                    $('.menu').each(function() {
+                        $(this).css("display","none");});
+                    $('#tabelM4').css("display","block");
+                });
+                    $('#M5').click(function(){
+                            $('.menu').each(function() {
+                                $(this).css("display","none");});
+                            $('#tabelM5').css("display","block");
+                        });
+
+
+
+})
+
+
+
+
+
