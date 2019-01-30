70cea66308832c4c15e69bef1599d074892333c8
mskahlous <mohamad.kahlous@capgemini.com>
2019-01-30 11:51:40 +0100

    Admin pages

--- src/main/java/com/molveno/molveno/hotel/room/RoomController.java
+++ src/main/java/com/molveno/molveno/hotel/room/RoomController.java
@@ -1,7 +1,29 @@
 package com.molveno.molveno.hotel.room;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @RestController
 public class RoomController {
+    @Autowired
+    RoomRepository roomRepository;
+
+    @RequestMapping(value = "/get-room",method = RequestMethod.GET)
+    public List<Room> getRoom(){
+        return roomRepository.findAll();
+    }
+
+//    @RequestMapping(value = "/add-room",method = RequestMethod.POST)
+//    public void saveRoom
+
+
+
+
 }
+
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,9 @@ INSERT INTO roomtype (name, price) VALUES ('standard', 100);
 INSERT INTO roomtype (name, price) VALUES ('luxury', 120);
 INSERT INTO room (roomtype, name) VALUES (1, 'room1');
 INSERT INTO room (roomtype, name) VALUES (2, 'room2');
+INSERT INTO room (roomtype, name) VALUES (3, 'room3');
+INSERT INTO room (roomtype, name) VALUES (3, 'room4');
+INSERT INTO room (roomtype, name) VALUES (2, 'room5');
+INSERT INTO room (roomtype, name) VALUES (1, 'room6');
+INSERT INTO room (roomtype, name) VALUES (2, 'room7');
+
